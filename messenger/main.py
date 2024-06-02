853f81d9c3f8b6834067f1d59364c7d2cee3bf4e
Sushmitha Popuri <sushmithapopuri@outlook.com>
2024-06-02 17:03:42 +0530

    Create main.py

---
+++ messenger/main.py
@@ -0,0 +1,28 @@
+import pywhatkit
+
+import time 
+import pyautogui
+# from pynput.keyboard import Key, Controller
+
+# keyboard = Controller()
+
+
+def send_whatsapp_message(msg: str):
+    try:
+        pywhatkit.sendwhatmsg_instantly(
+            phone_no="+xxxxxxxxx", 
+            message=msg,
+            tab_close=True
+        )
+        # time.sleep(10)
+        # pyautogui.click()
+        # time.sleep(2)
+        # keyboard.press(Key.enter)
+        # keyboard.release(Key.enter)
+        print("Message sent!")
+    except Exception as e:
+        print(str(e))
+
+
+if __name__ == "__main__":
+    send_whatsapp_message(msg="Test message from a Python script!")
